ebfa8f8be76843ab0993cb3100a17bfa9f486fd5
pravin jeurkar <jeurkarpravin96@gmail.com>
2020-12-06 00:09:49 +0530

    assignment two uploaded

--- AssignmentThree/QuestionThree/Program.cs
+++ AssignmentThree/QuestionThree/Program.cs
@@ -10,8 +10,76 @@ namespace QuestionThree
     {
         static void Main(string[] args)
         {
+            Student[] s=new Student[5];
+            
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter Student "+(i+1)+" details ");
 
+                Console.Write("Enter student name :: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter student Roll No :: ");
+                int rollno = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter student mark :: ");
+                decimal mark = Convert.ToDecimal(Console.ReadLine());
+                s[i] = new Student(name, rollno, mark);
+            }
+            Console.WriteLine();
+
+            foreach (Student s1 in s )
+            {
+                Console.WriteLine(s1.RollNo+" "+s1.Name+" "+s1.Mark);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
-    
+    public struct Student
+    {
+        private string name;
+        public string Name { 
+            set {
+                if (value != null)
+                    name = value;
+                }
+            get
+               {
+                return name;
+               }
+          }
+        private int rollNo;
+        public int RollNo
+        {
+            set
+            {
+                if (value > 0)
+                    rollNo = value;
+            }
+            get
+            {
+                return rollNo;
+            }
+        }
+        private decimal mark;
+        public decimal Mark
+        {
+            set
+            {
+                mark = value;
+            }
+            get
+            {
+                return mark;
+            }
+        }
+        public Student(string name=null,int rollNo=0,decimal mark=0)
+        {
+            this.name = "null";
+            this.mark = 0;
+            this.rollNo = 0;
+            this.Name = name;
+            this.Mark = mark;
+            this.RollNo = rollNo;
+        }
+    }
 }
--- AssignmentThree/QuestionTwo/Program.cs
+++ AssignmentThree/QuestionTwo/Program.cs
@@ -6,10 +6,70 @@ using System.Threading.Tasks;
 
 namespace QuestionTwo
 {
-    class Program
+
+    #region AssignmentThreeOfTwo
+    class Program1
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter number of batches in CDAC :: ");
+            int batch = Convert.ToInt32(Console.ReadLine());
+            
+
+            CDAC1[][] cdacBatch = new CDAC1[batch][];
+            
+            for (int i = 0; i < cdacBatch.Length; i++)
+            {
+          
+                Console.Write("Enter number of students in batch " + (i + 1)+" :: ");
+                int size = Convert.ToInt32(Console.ReadLine());
+                
+                cdacBatch[i] = new CDAC1[size];
+            }
+
+            for (int i = 0; i < cdacBatch.Length; i++)
+            {
+                Console.WriteLine("Enter a batch " + (i + 1) + " students details ");
+                for (int j = 0; j < cdacBatch[i].Length; j++)
+                {
+                    //Console.WriteLine("Enter a " + (j + 1) + " students details ");
+                    //Console.Write("Enter " + (j + 1) + " Student's PRN Number ");
+                    //double PRN = Convert.ToDouble(Console.ReadLine());
+                    //Console.Write("Enter " + (j + 1) + " Student's Name ");
+                    //string Name = Console.ReadLine();
+                    Console.Write("Enter Student "+(j+1)+" Marks :: ");
+                    int mark = Convert.ToInt32(Console.ReadLine());
+                    cdacBatch[i][j] = new CDAC1 { mark = mark };
+                  }
+              }
+            Console.WriteLine();
+            Console.WriteLine("=========================================================");
+            Console.WriteLine();
+            for (int i = 0; i < cdacBatch.Length; i++)
+            {
+                Console.WriteLine(" Batch " + (i + 1) + " students marks ");
+
+                for (int j = 0; j < cdacBatch[i].Length; j++)
+                {
+                    
+                    Console.WriteLine();
+                    Console.WriteLine(" Student ["+(j+1)+"] Mark :: "+cdacBatch[i][j].mark );
+                }
+                Console.WriteLine();
+                Console.WriteLine("=========================================================");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Please Enter a Key to continue......");
+            Console.ReadLine();
         }
+         
     }
+   public   class CDAC1
+    {
+      public int mark { set; get; }
+
+      }
+    #endregion
 }
